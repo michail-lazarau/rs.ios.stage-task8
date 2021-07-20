8699f05d33f9758c3c27335a66960201845a7ef7
Mikhail Lazarau (Michaux) <mikhail.lazarau1@gmail.com>
2021-07-20 23:35:30 +0300

    what

---
+++ michail-lazarau-Task8/michail-lazarau-Task8/Controllers/DrawingsViewController.m
@@ -0,0 +1,15 @@
+#import "DrawingsViewController.h"
+
+@interface DrawingsViewController ()
+
+@end
+
+@implementation DrawingsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+    self.view.backgroundColor = UIColor.orangeColor;
+}
+
+@end
