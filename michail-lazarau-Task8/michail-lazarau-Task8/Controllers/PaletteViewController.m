f0a1a6344c803c050a93ad02ae8cd832831943d6
Mikhail Lazarau (Michaux) <mikhail.lazarau1@gmail.com>
2021-07-24 22:12:07 +0300

    colors

--- michail-lazarau-Task8/michail-lazarau-Task8/Controllers/PaletteViewController.m
+++ michail-lazarau-Task8/michail-lazarau-Task8/Controllers/PaletteViewController.m
@@ -1,5 +1,8 @@
 #import "PaletteViewController.h"
 
+int const BUTTON_COUNT = 6;
+int const BUTTON_SPACING = 20;
+
 @interface PaletteViewController ()
 
 @property(nonatomic,strong) UIButton *saveBtn;
@@ -13,6 +16,7 @@
     // Do any additional setup after loading the view.
     [self setupPaletteView];
     [self setupSaveButton];
+    [self setupColorButtons];
 }
 
 -(void)setupPaletteView {
@@ -35,6 +39,37 @@
     [self.view addSubview:self.saveBtn];
 }
 
+- (void) setupColorButtons {
+    
+    NSArray<UIColor *> *colorsFirstRow = [[NSArray alloc] initWithObjects:[UIColor colorNamed:@"rsRed"], [UIColor colorNamed:@"rsBlue"], [UIColor colorNamed:@"rsGreen"], [UIColor colorNamed:@"rsGray"], [UIColor colorNamed:@"rsPurple"], [UIColor colorNamed:@"rsOrange"], [UIColor colorNamed:@"rsYellow"], [UIColor colorNamed:@"rsLightBlue"], [UIColor colorNamed:@"rsPink"], [UIColor colorNamed:@"sortOfRsBlack"], [UIColor colorNamed:@"rsDarkGray"], [UIColor colorNamed:@"rsBrown"], nil];
+    
+    CGFloat buttonSize = 40;
+    CGFloat colorLayerSize = 24;
+    
+    UIButton *button = [[UIButton alloc] init];
+    button.layer.cornerRadius = 10.0f;
+    button.layer.masksToBounds = true;
+    
+    // добавить кнопки на вью - 1ый ряд
+    for (int i = 0; i < BUTTON_COUNT; i++) {
+
+        button.frame = CGRectMake(i * (BUTTON_SPACING + buttonSize) + 17, 92, buttonSize, buttonSize);
+//        UIButton *button = [[UIButton alloc] initWithFrame: CGRectMake(i * (BUTTON_SPACING + buttonSize) + 17, 92, buttonSize, buttonSize)];
+        button.tag = i;
+        button.backgroundColor = [UIColor colorNamed:@"White"];
+        
+        CALayer *sublayer = [CALayer layer];
+        sublayer.frame = CGRectMake(8, 8, colorLayerSize, colorLayerSize);
+        sublayer.cornerRadius = 10.0f;
+        sublayer.masksToBounds = true;
+        sublayer.backgroundColor = colorsFirstRow[i].CGColor;
+        [button.layer addSublayer: sublayer];
+        
+        [button addTarget:self action:@selector(colorButtonWasTapped) forControlEvents:UIControlEventTouchUpInside];
+        [self.view addSubview:button];
+    }
+}
+
 - (void)saveButtonWasTapped {
     [UIView animateWithDuration:0.27 animations:^(void){
             self.view.frame = CGRectMake(0, self.view.bounds.size.height, self.view.bounds.size.width, self.view.bounds.size.height);
@@ -45,7 +80,7 @@
     }];
 }
 
-- (void) setupColorButtons {
+- (void)colorButtonWasTapped {
     
 }
 
